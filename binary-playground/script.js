e5729507bdf6935eb9d73ec6af8b5609c240b115
hyphen-underscore-dash <kristenccwong@gmail.com>
2022-04-22 22:22:48 +0800

    a bunch of stuff, see changelog

--- binary-playground/script.js
+++ binary-playground/script.js
@@ -3,22 +3,24 @@ $(document).ready(function(){
     var int_length = 4;
     var cur_length = 4;//change changeLength function: add the upper bits so that lower bit values can be 
     //preserved
-    var dec_value = 0;//decimal value
+    var dec_value = 0;
     var bits = [0,0,0,0]; // for calculating things
-    //add custom animation or sound effects onlick?
+    var bits2 = [0,0,0,0];// electric boogaloo
+    
+    //addsound effects onlick?
 
     //for independent bit, changes text only
     function switchBit(event) 
     
     {
-        if ($(this).html() == 0) 
+        if ($(this).text() == 0) 
         {
-            $(this).html(1);
+            $(this).text(1);
             $(this).attr("class", "bits one bit-changer");
         }
         else
         {
-            $(this).html(0);
+            $(this).text(0);
             $(this).attr("class", "bits zero bit-changer");
         }
     }
@@ -42,7 +44,7 @@ $(document).ready(function(){
         {
             $(".bits").html(b);
             $(".bits").attr("class", `bits ${bit} bit-changer`);
-            bits.push[b];
+            bits.push(b);
         }
     }
 
@@ -88,11 +90,12 @@ $(document).ready(function(){
         cur_length = l;
         
         //console.log($(".bits").css("width"));
-        console.log("current integer length is", int_length);
+        console.log("length changed to", int_length);
         
     }
 
     //for operators, calculates for array
+    //Shouldnt it be the other way around?
     function updateValue(event)
     {
         dec_value = 0;
@@ -101,7 +104,7 @@ $(document).ready(function(){
         $("#bit-container").find(".bits").each(function(i) {
             bits.push(parseInt($(this).text()));
         });
-        console.log(bits);
+        console.log("updated: ", bits);
 
         //Calculate finally
         for (let p=0, n=bits.length; p<n; p++) // power
@@ -112,6 +115,47 @@ $(document).ready(function(){
         $("#dec-display").text((`Decimal value: ${dec_value}`));
     }
     
+    //for the negate button, changes the array
+    function negateBits(event)
+    {
+        bits = [];
+        for (let i=0; i<int_length; i++)
+        {
+            if ($(`#bit${i}`).text() == 0)
+            {
+                $(`#bit${i}`).text(1);
+                $(`#bit${i}`).attr("class", "bits one bit-changer");
+                bits.push(1);
+            }
+            else{
+                $(`#bit${i}`).text(0);
+                $(`#bit${i}`).attr("class", "bits zero bit-changer");
+                bits.push(0);
+            }
+
+
+        }
+    }
+
+    function shiftBits(event)
+    {
+        let isLeft = event.data.left;// true for left, false for right
+
+        //let keys = [(event.which == 37), (event.which == 39)];
+        if (isLeft)
+        {
+            bits.shift(1);
+            bits.push(0);
+        }
+        else
+        {
+            bits.unshift(0);
+            bits.pop();
+        }
+
+        console.log("shifted:", bits);
+
+    }
     // listeners
     for (let i=4; i<=32; i+=4)
     {
@@ -119,10 +163,16 @@ $(document).ready(function(){
         $(document).on("click", `#${i}bit`, {l:i}, changeLength);
 
     }
-    $(document).on("click", ".bits", switchBit);
+
+    $(document).on("click", ".bits", switchBit);// THE better listener adder
+    //$(document).on("keydown", shiftBits)
+    $(document).on("click", "#bitshift-left", {left: true}, shiftBits);
+    $(document).on("click", "#bitshift-right", {left: false}, shiftBits);
+    $(document).on("click", "#bit-negate", negateBits);
     $(document).on("click", "#set-on", {b:1}, setAllBits);
     $(document).on("click", "#set-off", {b:0}, setAllBits);
-    $(document).on("click", ".bit-changer", updateValue);// THE better listener adder
+    $(document).on("click", ".bit-changer", updateValue);// place this at the bottom idk y
+
 
 
     /*$(window).on("resize", function()
