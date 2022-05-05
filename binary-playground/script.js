1781aacbcb1b437d991437a1d9799854c6a6f3ad
kr15w <kristenccwong@gmail.com>
2022-05-05 22:18:10 +0800

    added signs yay

--- binary-playground/script.js
+++ binary-playground/script.js
@@ -1,78 +1,93 @@
-// here i go green baeby
 $(document).ready(function(){
-    var bit_w = parseInt($(".bits").css("width"));//In px
-    var int_length = 4;
-    //preserved
-    var dec_value = 0;
-    var bits = [0,0,0,0]; // for calculating things
-    var bits2 = [0,0,0,0];// electric boogaloo
-    
-    //addsound effects onlick?
+    //alert(window.localStorage.getItem("num1"));
+    // how to use this
 
-    //for independent bits
-    //add this listener to a for loop across all #bitN
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent))
+    // foun dthemob ile use r
+    {
+        window.location.replace("stinky.html");
+    }
+
+    var bit_w = parseInt($(".bits").css("width"));//In px
+    var int_length = 4; // preserved
+    
+    var dec_valueA = 0;
+    var dec_valueB = 0;
+    var dec_valueC = 0;
+
+    var bin1 = 0// int.toString(2);
+    var bitsA = [0,0,0,0]; // for calculating things
+    var bitsB = [0,0,0,0];// electric boogaloo
+    var isSigned = $('#sign-box').is(":checked");
+    
+    // addsound effects onlick?
+
+    // listeners
+
+    // for independent bits
+    // add this listener to a for loop across all #bitN
     function switchBit(event) 
     {
         let i = event.data.i;
-        bits[i] = 1 - bits[i];
+        bitsA[i] = 1 - bitsA[i];
     }
 
     //for resetters
     function setAllBits(event)
     {
         let b = event.data.b;// integer
-        for (let i=0, n=bits.length; i<n; i++)
+        for (let i=0, n=bitsA.length; i<n; i++)
         {
-            bits[i] = b;
+            bitsA[i] = b;
         }
     }
 
-    //for length menu, changed array and display
+    //for length menu, changes array and display
     function changeLength(event)
     {
         let l = event.data.l;// target length
         int_length = l;// for displaying purposes only
 
         //changes the length of the array, keeps lower bits
-        if (bits.length < l) 
+        if (bitsA.length < l) 
         {
-            for (let i=0, n=l-bits.length; i<n; i++)
+            for (let i=0, n=l-bitsA.length; i<n; i++)
             {
-                bits.unshift(0);
+                bitsA.unshift(0);
             }
         }
-        else if (bits.length > l) 
+        else if (bitsA.length > l) 
         {
-            for (let i=0, n=bits.length-l; i<n; i++)
+            for (let i=0, n=bitsA.length-l; i<n; i++)
             {
-                bits.shift();
+                bitsA.shift();
             }
         }
 
-        $("#bit-container").empty();
-
-        for (let i=0; i<l/4-1; i++)
+        $("#bit-container-a").empty();
+        for (let i=0; i<l/4; i++)
         {
             for (let j=0; j<4; j++)
             {
-                $("#bit-container").append(`<div class="bits zero bit-changer" id="bit${i*4+j}">0</div>`);
+                $("#bit-container-a").append(`<div class="bits zero bit-changer" id="bit-a${i*4+j}">0</div>`);
                 
             }
-            $("#bit-container").append("<pre>  </pre>");
+            // seperator
+            if (!(i == l/4-1))
+            {
+                $("#bit-container-a").append("<pre>  </pre>");
+            }
+            
         }
-        // The final 4 bits
-        for (let i=l-4; i<l; i++)
-        {
-            $("#bit-container").append(`<div class="bits zero bit-changer" id="bit${i}">0</div>`);
-        }
-          
+        
+        
 
         // if the total width of the bits (and the space) is too large, shrink them
-        if ((50 + 2) * l >= $(window).width())
+        /*if ((50 + 2) * l >= $(window).width())
         {
             console.log("Too big!");
             bit_w = 20;
-            $("#bit-container").css("gap", "2px");
+            $("#bit-container-a").css("gap", "2px");
         }
         else
         {
@@ -80,7 +95,7 @@ $(document).ready(function(){
         }
         $(".bits").css("width", `${bit_w}px`);
         $(".bits").css("font-size",`${bit_w*2}px`);
-        bit_w = 50; // brh
+        bit_w = 50; // brh*/
 
         //cur_length = l;
         
@@ -92,9 +107,9 @@ $(document).ready(function(){
     //for the negate button
     function negateBits(event)
     {
-        for (let i=0, n=bits.length; i<n; i++)
+        for (let i=0, n=bitsA.length; i<n; i++)
         {
-            bits[i] = 1 - bits[i];
+            bitsA[i] = 1 - bitsA[i];
         }
 
     }
@@ -106,40 +121,90 @@ $(document).ready(function(){
         //let keys = [(event.which == 37), (event.which == 39)];
         if (isLeft)
         {
-            bits.shift(1);
-            bits.push(0);
+            bitsA.shift(1);
+            bitsA.push(0);
         }
         else
         {
-            bits.unshift(0);
-            bits.pop();
+            bitsA.unshift(0);
+            bitsA.pop();
         }
 
-        console.log("shifted:", bits);
+        console.log("shifted:", bitsA);
 
     }
 
-    //for operators, updates the display according to array
+    function crement(event)
+    // add or subtract the number by 1
+    {
+        let amt = event.data.a;
+        ;
+    }
+    //for operators, updates the display according to array and the decimal value
+    //stores numbers into localstorage
     function updateValue(event)
     {
-        dec_value = 0;
-        for (let i=0, n=bits.length; i<n; i++) 
-        {
-            dec_value += (2**(n-1 - i)) * bits[i]
-
-            $(`#bit${i}`).text(bits[i]);
-
-            if (bits[i]) // is one
+        dec_valueA = 0;
+        dec_valueB = 0;
+        
+        //calculate for decimal
+        //clean this up [!]
+        if (isSigned && bitsA[0] == 1)
+        {   
+            for (let i=0, n=bitsA.length; i<n; i++)
             {
-                $(`#bit${i}`).attr("class", "bits one bit-changer");
+                dec_valueA += 2**(n-1-i) * (1-bitsA[i]);
+            }
+            dec_valueA++;
+            dec_valueA *= (-1) ** bitsA[0];
+        }
+        else
+        {
+            for (let i=0, n=bitsA.length; i<n; i++) 
+            {
+                dec_valueA += (2**(n-1 - i)) * bitsA[i]
+            }
+        }
+            // multiples by -1 if first bit is 1
+
+        //update display
+        
+        for (let i=0, n=bitsA.length; i<n; i++) 
+        {
+            $(`#bit-a${i}`).text(bitsA[i]);
+
+            if (bitsA[i] == 1)
+            {
+                $(`#bit-a${i}`).attr("class", "bits one bit-changer");
             }
             else
             {
-                $(`#bit${i}`).attr("class", "bits zero bit-changer");
+                $(`#bit-a${i}`).attr("class", "bits zero bit-changer");
             }
         }
-        $("#dec-display").text(`Decimal value: ${dec_value}`);
-        console.log("bit1: ", bits);
+
+        if (isSigned)
+        {
+            $("#bit-a0").addClass("sign");
+        }
+        else
+        {
+            $("#bit-a0").removeClass("sign");
+        }
+        
+        //debug
+        $("#dec-display").text(`Decimal value: ${dec_valueA}`);
+        console.log("bit1: ", bitsA);
+
+        window.localStorage.setItem("num1", dec_valueA);
+        window.localStorage.setItem("num2", dec_valueB);
+
+    }
+
+    // restore "cookies" (not really)
+    if (isSigned)
+    {
+        updateValue();
     }
     // listeners
     // THE better listener adder
@@ -152,26 +217,26 @@ $(document).ready(function(){
 
     for (let j=0, n=32; j<n; j++) // bruteforce it bruh
     {
-        $(document).on("click", `#bit${j}`, {i:j}, switchBit);
+        $(document).on("click", `#bit-a${j}`, {i:j}, switchBit);
     }
 
 
     //$(document).on("keydown", shiftBits)
+    $(document).on("click", "#sign-box", function() {
+        isSigned = $('#sign-box').is(":checked");
+        console.log("sign toggled!");
+    })
     $(document).on("click", "#bitshift-left", {left: true}, shiftBits);
     $(document).on("click", "#bitshift-right", {left: false}, shiftBits);
     $(document).on("click", "#bit-negate", negateBits);
     $(document).on("click", "#set-on", {b:1}, setAllBits);
     $(document).on("click", "#set-off", {b:0}, setAllBits);
-    $(document).on("click", ".bit-changer", updateValue);// place this at the bottom idk y
+    $(document).on("click", ".bit-changer", updateValue);// place this at the bottom
 
-
-
-    /*$(window).on("resize", function()
-    {
-        $(window).width() = $(window).width();
-    });*/
     
 
 });
 
 
+//"j."
+//     ~apandah
