95a47a4a7a436f2363e1a47916d82c3da7e44106
kristen wong <kristenccwong@gmail.com>
2026-03-22 23:46:29 +0800

    react support ig

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>kris womg</h1>
+      <sub>a lawyer you can trust</sub>
+      <p>hello i code and draw</p>
+      <nav> email(captcha) insta gih</nav>
+      <hr />
+      <h2>projects</h2>
+      <a href="https://krispyre.dev/dualpfp">dualpfp, wip</a>
+      <hr />
+      <a href="https://www.teamcherry.com.au">im silking it so hard rn</a>
+    </>
+  );
+}
+
+export default App;
